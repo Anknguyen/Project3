02ea32937295b098b4f9aea7a80fed2df11af497
An <akimnguyen1999@gmail.com>
2023-05-10 19:49:00 -0400

    localhost

--- project3/src/server/connection.js
+++ project3/src/server/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require('mongoose');
-const uri = 'mongodb://localhost:27017/prj3-db';
+const uri = 'mongodb://127.0.0.1:27017/prj3-db';
 connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
